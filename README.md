7b492aff1d680186edd54ff03d1cde76828d45ac
Miriele Soares <miriele.soares123@gmail.com>
2024-05-05 12:13:18 -0300

    Subindo projeto

---
+++ README.md
@@ -0,0 +1,15 @@
+## Apresentação
+Olá! Seja bem-vindo(a) ao projeto "Events"! Agradeço por estar aqui!
+
+Este projeto foi criado utilizando o Laravel, com o objetivo de aprimorar minhas habilidades no framework. Neste projeto os usuários podem adicionar eventos sobre uma variedade de assuntos, visualizá-los e confirmar sua participação em eventos de outros usuários. Além disso, é possível editar, remover eventos e cancelar inscrições. O sistema é completo, aproveitando as tecnologias oferecidas pelo Laravel.
+
+## Passos para Utilizar:
+1. Inicie o banco de dados utilizando XAMPP.
+2. Se necessário, altere as credenciais e informações do banco de dados, como nome, porta, nome de usuário, etc., no arquivo `.env` localizado na raiz do projeto (linhas 22 à 27).
+3. Execute as migrações do banco de dados com o comando: `php artisan migrate`. Se tudo ocorrer conforme o esperado, as tabelas serão criadas no seu banco de dados.
+4. Abra o terminal do VS Code e execute o comando: `php artisan serve`.
+5. Acesse a URL gerada pelo comando.
+
+## Créditos
+Desenvolvi este projeto durante o curso "Curso Laravel", ministrado por Matheus Battisti, disponibilizado no canal "Matheus Battisti - Hora de Codar" no YouTube.
+link do repositório: https://github.com/matheusbattisti/curso_laravel
---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\User;
+
+class EventController extends Controller
+{
+
+    public function index()
+    {
+        $search = request('search');
+
+        if ($search)
+            $events = Event::where([['title', 'like', '%' . $search . '%']])->get();
+
+        if (!$search)
+            $events = Event::all();
+
+        return view('welcome', [
+            'events' => $events,
+            'search' => $search
+        ]);
+    }
+
+    public function create()
+    {
+        return view('events.create');
+    }
+
+    public function store(Request $request)
+    {
+        $event = new Event;
+
+        $event->date = $request->date;
+        $event->city = $request->city;
+        $event->title = $request->title;
+        $event->items = $request->items;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $imageName = $this->imageUpload($request);
+            $event->image = $imageName;
+        }
+
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if ($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach ($userEvents as $userEvent) {
+
+                if ($userEvent['id'] == $id)
+                    $hasUserJoined = true;
+            }
+        }
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', [
+            'event' => $event,
+            'eventOwner' => $eventOwner,
+            'hasUserJoined' => $hasUserJoined
+        ]);
+    }
+
+    public function dashboard()
+    {
+        $user = auth()->user();
+        $events = $user->events;
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', [
+            'events' => $events,
+            'eventsasparticipant' => $eventsAsParticipant
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $event = Event::findOrFail($id);
+        $event->users()->detach();
+        $event->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
+    }
+
+    public function edit($id)
+    {
+        $user = auth()->user();
+        $event = Event::findOrFail($id);
+
+        if ($user->id != $event->user_id)
+            return redirect('/dashboard');
+
+        return view('events.edit', [
+            'event' => $event
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $imageName = $this->imageUpload($request);
+            $data['image'] = $imageName;
+        }
+
+        Event::findOrFail($request->id)->update($data);
+
+        return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
+    }
+
+    public function joinEvent($id)
+    {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->attach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
+    }
+
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento: ' . $event->title);
+    }
+
+    function imageUpload($request)
+    {
+        $requestImage = $request->image;
+        $extension = $requestImage->extension();
+        $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+        $requestImage->move(public_path('img/events'), $imageName);
+
+        return $imageName;
+    }
+}
